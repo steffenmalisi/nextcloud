4a9a140fd62bc028c06db0029e6581bfea823cdc
steffenmalisi <steffenmalisi@users.noreply.github.com>
2023-05-09 22:04:08 +0200

    Adds podman config and services

---
+++ podman/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eu
+
+function generate_password {
+  openssl rand -base64 32 | sed -e '$s/[]\/$*.^[]/\\&/g';
+}
+
+# copy config
+cp -r config /var/config
+
+# generate nextcloud secrets
+if ! [ -e /etc/sysconfig/nextcloud ]; then
+  sed -e "s/<NEXTCLOUD_ADMIN_PASSWORD>/\"$(generate_password)\"/g" services/nextcloud/.env.template \
+    | sed -e "s/<NEXTCLOUD_SERVERINFO_TOKEN>/\"$(generate_password)\"/g" \
+    | sed -e "s/<POSTGRES_PASSWORD>/\"$(generate_password)\"/g" \
+    | sed -e "s/<REDIS_PASSWORD>/\"$(openssl rand -hex 32)\"/g" \
+    | sed -e "s/<GRAFANA_ADMIN_PASSWORD>/\"$(generate_password)\"/g" \
+    > /etc/sysconfig/nextcloud
+  chmod 400 /etc/sysconfig/nextcloud
+fi
+
+# generate monitoring secrets
+if ! [ -e /etc/sysconfig/monitoring ]; then
+  sed -e "s/<GRAFANA_ADMIN_PASSWORD>/\"$(generate_password)\"/g" services/monitoring/.env.template \
+    > /etc/sysconfig/monitoring
+  chmod 400 /etc/sysconfig/monitoring
+fi
+
+# deploy nextcloud
+cp services/nextcloud/*.service /etc/systemd/system/
+
+# deploy monitoring
+cp services/monitoring/*.service /etc/systemd/system/
