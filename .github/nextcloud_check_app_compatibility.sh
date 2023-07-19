93b6687c444c0392810f27ceb3d2a26a7433560d
steffenmalisi <steffenmalisi@users.noreply.github.com>
2023-07-19 08:56:36 +0200

    Check app compatiblity

---
+++ .github/nextcloud_check_app_compatibility.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+DOCKERFILE=$1
+APPS=$2
+PLATFORM=$(grep 'FROM nextcloud' "$DOCKERFILE"  | awk -F ':' '{print $2}' | awk -F '-' '{print $1}')
+APP_IDS=$(curl -s https://apps.nextcloud.com/api/v1/platform/"$PLATFORM"/apps.json | jq -r '.[].id')
+COMPATIBLE=true
+
+read -ra APPS_ARRAY <<< "$APPS"
+for app in "${APPS_ARRAY[@]}"; do
+  if ! grep -q "$app" <<< "$APP_IDS"; then
+    echo "$app is not compatible with $PLATFORM"
+    COMPATIBLE=false
+  fi
+done
+
+if [ "$COMPATIBLE" = false ]; then
+  exit 1
+else
+  echo "All apps are compatible with $PLATFORM"
+fi
+
